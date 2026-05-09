0ec863b7d800e7115cc80cfca6cc4f300e7dd2d9
Entony <franciscoentonydev@gmail.com>
2026-05-08 21:50:13 -0300

    Configura deploy para GitHub Pages

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/simplified-data-structures",
   compiler: {
     styledComponents: true,
   },
